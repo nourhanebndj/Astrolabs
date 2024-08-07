3a9ced25da20037716d7bfa8da5aea232a7f12b2
Hi👋, I'm Nourhane <133056977+nourhanebndj@users.noreply.github.com>
2024-08-08 00:09:46 +0100

    Astrolabs

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuxqeuLEnWBNjhaV3vGqnIflDO8W1NFSU",
-  authDomain: "astrolabs-1a622.firebaseapp.com",
-  projectId: "astrolabs-1a622",
-  storageBucket: "astrolabs-1a622.appspot.com",
-  messagingSenderId: "42001484814",
-  appId: "1:42001484814:web:0f118be52a6f6f9dd7005f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 let app;
